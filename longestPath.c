7baf65286eefe885e2ee779abc92d3bf1dcbd7ee
Sephta <seth.tal5233@gmail.com>
2020-01-27 13:46:18 -0800

    Added: struct Noded, struct Edge, Read_Matrix_Data(). TODO: make separate version that uses stdin instead of file/io

--- longestPath.c
+++ longestPath.c
@@ -19,9 +19,61 @@ void usage(int argc, char** argv) {
     }
 }
 
+/* Node Struct
+ * A Node contains a value and a list of its neighbors
+*/
+typedef struct Node {
+    int val;          // value of current node
+    struct Node* neighbors;  // list of node's neighbors
+} Node;
+
+/* Edge Struct
+ * An edge contains two values representing starting node and ending node
+*/
+typedef struct Edge {
+    int start;  // Start of edge
+    int end;    // End of edge
+    int w;      // weight of edge between start -> end
+} Edge;
+
+// Todo: malloc the graph 
+void Malloc_Graph();
+
+// Todo: Read in data from input file ...
+void Read_Matrix_Data(int *n, int *m, int** start, int** end, int** weight, FILE** input) {
+    /* First line of the file contains two numbers N, and M
+       * N : number of nodes
+       * M : number of edges
+    */
+    fscanf(*input, "%d %d", n, m);
+
+    /* Now I malloc the arrays passed into Read_Matrix_Data
+       * start : a list of the starting nodes for a given edge
+       * end : a list of the end node for a given edge
+       * weight : a list of weights for a given edge
+    */
+    *start = (int *) malloc(sizeof(int) * *m);
+    *end = (int *) malloc(sizeof(int) * *m);
+    *weight = (int *) malloc(sizeof(int) * *m);
+
+    // Assign values
+    for (int i = 0; i < *m; i++) {
+        fscanf(*input, "%d %d %d", &(*start)[i], &(*end)[i], &(*weight)[i]);
+    }
+
+    // ! TO BE REMOVED LATER:
+    /* This was for bug testing...
+    // printf("%d %d\n", *n, *m);
+    // for (int j = 0; j < *m; j++) {
+    //     printf("%d %d %d\n", (*start)[j], (*end)[j], (*weight)[j]);
+    // }
+    */
+}
+
 // Todo: T-Sort
 void TopologicalSort();
 
+// main is where the whole program executes
 int main(int argc, char** argv) {    
     // function used to define usage of program
     usage(argc, argv); 
@@ -34,16 +86,55 @@ int main(int argc, char** argv) {
     }
 
     char matrix_name[MAX_FILENAME_LEN];  // init char *
-    strcpy(matrix_name, argv[1]);         // copies over file_name to a char*
+    strcpy(matrix_name, argv[1]);        // copies over file_name to a char*
 
-    
+    int n;  // Number of Nodes
+    int m;  // Number of Edges
+    int* start_nodes;
+    int* end_nodes;
+    int* weight;
+
     /* ----------------------------------------------------------------------------------------- */
     // Beginning stdout stream ... 
     printf("\n");
     fprintf(stdout, "Beginning read of Matrix : <%s> ... \n", matrix_name);
 
-    // Todo: stuff ... 
+    // Todo: using file/io read in data from input file and store in some arr
 
+    // ! start_nodes = malloc(sizeof(int) * m);
+    // ! end_nodes = malloc(sizeof(int) * m);
+    // ! weight = malloc(sizeof(int) * m);
+
+    FILE* input = fopen(argv[1], "r");  // Opens input file for read only
+
+    if (input == NULL) {
+        fprintf(stderr, "ERR: could not open file <%s>\n", matrix_name);
+        exit(EXIT_FAILURE);
+    }
+
+    Read_Matrix_Data(&n, &m, &start_nodes, &end_nodes, &weight, &input);
+
+    assert(start_nodes);
+    assert(end_nodes);
+    assert(weight);
+
+    fclose(input);
+
+    // Todo: malloc necessary items from the arr to the 'graph'
+
+    Malloc_Graph();
+
+    //Todo: stuff ...
+
+    // End of stout stream.
+    /* ----------------------------------------------------------------------------------------- */
     printf("\n");
+
+    // ! FREE anything malloc'd bellow...
+
+    free(start_nodes);
+    free(end_nodes);
+    free(weight);
+
     return 0;
 }
