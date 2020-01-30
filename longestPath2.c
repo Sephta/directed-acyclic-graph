fd7e122670c37f4bf0782402171692c14d2d6413
Sephta <seth.tal5233@gmail.com>
2020-01-29 19:13:10 -0800

    Finalized longestPath2 to turn in as completed homework. Added Free_P_Tree function to free the malloc'd 2d array of Node structs. Also added acknowledgements and credit to individuals that assisted me on this project through discussion.

--- longestPath2.c
+++ longestPath2.c
@@ -2,6 +2,13 @@
 Seth Tal
 01/26/2020
 Assignment02 ~ Find longest path in Directed Acyclic Graph and number of longest paths of same length
+
+Acknowledgements / Credit:
+I received assistance through discussion from the following people...
+> Zhangxiang Hu
+> Abduarraheem Elfandi
+
+The only outside recourses used to complete this project are class notes and lecture slides from the website
 */
 
 #include <stdio.h>
@@ -18,13 +25,9 @@ Assignment02 ~ Find longest path in Directed Acyclic Graph and number of longest
 */
 typedef struct Node {
     int val;  // value of current node
-    int lp;   // current longest path value (by weight)
-    int nlp;  // current number of longest paths
     int w;    // if node is a predecessor then w represents weight of edge from sucessor -> node
-    // struct Node* next;
 } Node;
 
-// Todo: add comments
 void Malloc_P_Tree(int n, Node*** p) {
     // malloc the predecessor node array
     
@@ -37,29 +40,24 @@ void Malloc_P_Tree(int n, Node*** p) {
                 (*p)[i][j].val = i + 1;
             else
                 (*p)[i][j].val = -1;
-
-            if (i == 0 && j == 0)
-                (*p)[i][j].lp = 0;
-            else
-                (*p)[i][j].lp = BIG_NEGATIVE_NUMBER;
-
-            (*p)[i][j].nlp = 1;
             (*p)[i][j].w = 0;
         }
     }
-
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         printf(" %d ", p[i][j].val);
-    //     }
-    //     printf("\n");
-    // }
 }
 
-// Todo: add comments
+void Free_P_Tree(int n, Node** p) {
+    for (int i = 0; i < n; i++) {
+        Node* current_ptr = p[i];
+        free(current_ptr);
+    }
+}
+
+/*
+ * This function simply initializes the adjaceny matrix defined in main()
+*/
 void Build_P_Tree(int n, int m, int* start, int* end, int* weight, Node** p) {
-    // int next_pred = 1;
     for (int i = 0; i < m; i++) {
+        // This is a small optimization such that the calculation does not need to be calculated when compiled 3n times
         int curr_node = end[i] - 1;
         for (int j = 0; j < n; j++) {
             if (p[curr_node][j].val == -1) {
@@ -69,26 +67,17 @@ void Build_P_Tree(int n, int m, int* start, int* end, int* weight, Node** p) {
             }
         }
     }
-
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < n; j++) {
-    //         printf(" %d ", p[i][j].val);
-    //     }
-    //     printf("\n");
-    // }
 }
 
-// Todo: add comments
+/*
+ * i = current node
+ * j = current predecessor
+ * w = weight
+ * val = node value
+ * lp[i] = longest path of i'th node
+ * nlp[i] = number of longest paths to i'th node
+*/
 void Process_P_Tree(int n, int m, int* lp, int* nlp, Node** p) {
-    /*
-     * i = current node
-     * j = current predecessor
-     * w = weight
-     * val = node value
-     * lp[i] = longest path of i'th node
-     * nlp[i] = number of longest paths to i'th node
-    */
-
     // from node 1 till node n
     for (int i = 0; i < n; i++) {
         // from predecessor 1 till n
@@ -114,7 +103,9 @@ void Process_P_Tree(int n, int m, int* lp, int* nlp, Node** p) {
     }
 }
 
-// Todo: Read in data from input file ...
+/*
+ * This function reads the data from stdin and stores into arrays
+*/
 void Read_Matrix_Data(int *n, int *m, int** start, int** end, int** weight) {
     /* First line of the file contains two numbers N, and M
        * N : number of nodes
@@ -135,32 +126,26 @@ void Read_Matrix_Data(int *n, int *m, int** start, int** end, int** weight) {
     for (int i = 0; i < *m; i++) {
         fscanf(stdin, "%d %d %d", &(*start)[i], &(*end)[i], &(*weight)[i]);
     }
-
-    // ! TO BE REMOVED LATER:
-    // printf("Grabbing graph data from stdin...\n");
-    // printf(".\n");
-    // printf(". N : %d\n. M : %d\n", *n, *m);
-    // printf(".\n");
-    // for (int j = 0; j < *m; j++)
-    //     printf(". . %d -> %d . W : %d\n", (*start)[j], (*end)[j], (*weight)[j]);
-    // printf(".\n");
-    // printf(". Done\n");
-    // printf("\n");
 }
 
-// main is where the whole program executes
-int main() {    
-    // function used to define usage of program
+/*
+ * main is where the whole program executes
+*/ 
+int main() {
 
+    // function used to define usage of program
     int n;             // Number of Nodes
     int m;             // Number of Edges
     int* start_nodes;  // list of starting nodes
     int* end_nodes;    // list of ending nodes
     int* weight;       // weight of edge from start_nodes[i] -> end_nodes[i]
 
-    int* lp_arr;           // longest path array. Each index corresponds to a node on the graph. The i'th node corresponds to the i'th index of the array
-    int* num_lp_arr;          // Same logic as above.
-    Node** p_tree;
+    int* lp_arr;       // longest path array. Each index corresponds to a node on the graph. The i'th node corresponds to the i'th index of the array
+    int* num_lp_arr;   // Same logic as above.
+    Node** p_tree;     // the adjacency list I built is a literal matrix
+
+    /*
+    */
 
     /* ----------------------------------------------------------------------------------------- */
     // Beginning stdout stream ... 
@@ -175,16 +160,24 @@ int main() {
     lp_arr = malloc(sizeof(int) * n);
     num_lp_arr = malloc(sizeof(int) * n);
 
-    // Initialize all values in lp_arr to 0 and num_lp_arr to 1
+    // Initialize the longest path array and num longest path arrays
     for (int i = 0; i < n; i++) {
-        lp_arr[i] = 0;
-        num_lp_arr[i] = 1;
+        // The starting node will always initialize to lp[0] = 0, nlp[0] = 1
+        if (i == 0) {
+            lp_arr[i] = 0;
+            num_lp_arr[i] = 1;
+        } else {
+            lp_arr[i] = BIG_NEGATIVE_NUMBER;
+            num_lp_arr[i] = 0;
+        }
     }
 
     Malloc_P_Tree(n, &p_tree);
 
+    // Here is where I simply build my adjacency list
     Build_P_Tree(n, m, start_nodes, end_nodes, weight, p_tree);
 
+    // This is where I implemented a longest path algorithm
     Process_P_Tree(n, m, lp_arr, num_lp_arr, p_tree);
 
     fprintf(stdout, "longest path: %d\nnumber of longest paths: %d\n", lp_arr[n-1], num_lp_arr[n-1]);
@@ -192,10 +185,16 @@ int main() {
     // End of stout stream.
     /* ----------------------------------------------------------------------------------------- */
 
-    // ! FREE anything malloc'd bellow...
+    // Here I free any and all malloc'd data bellow...
     free(start_nodes);
     free(end_nodes);
     free(weight);
+
+    Free_P_Tree(n, p_tree);
+    free(p_tree);
+
+    free(lp_arr);
+    free(num_lp_arr);
 
     return 0;
 }
